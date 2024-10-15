513f12424466c10590d4b6b49f621115269b5211
News-s <84093019+news-s@users.noreply.github.com>
2024-10-15 16:26:50 +0200

    first commit

---
+++ stop.py
@@ -0,0 +1,31 @@
+import psutil
+from time import sleep
+import tkinter as tk
+from tkinter import messagebox
+
+# Nazwa programu, którego uruchomienie chcesz monitorować
+TARGET_PROGRAM = ["VALORANT.exe", "chrome.exe", "LeagueClient.exe", "Discord.exe", "Riot Client.exe"]  # Zmień na nazwę programu, który chcesz monitorować
+exc_list = []
+
+def is_program_running(program_name):
+    for proc in psutil.process_iter(['name']):
+        name = proc.info['name']
+        if name in program_name:
+            if name not in exc_list:
+                return True
+    return False
+
+def show_popup():
+    root = tk.Tk()
+    root.withdraw()  # Ukrywa główne okno
+    messagebox.showinfo("Program uruchomiony", f"{TARGET_PROGRAM} został uruchomiony! Czy jesteś pewien, że potrzebujesz z niego korzystać?")
+    root.destroy()  # Zamyka okno po potwierdzeniu
+
+if __name__ == "__main__":
+    print("Monitoring uruchomienia programu...")
+
+    # Pętla, która monitoruje uruchomienie programu
+    while True:
+        if is_program_running(TARGET_PROGRAM):
+            show_popup()
+        sleep(5)
